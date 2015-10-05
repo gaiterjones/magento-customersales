af7dd593333ef335a2c1016b025fee5a67992181
gaiterjones <paj@gaiterjones.com>
2015-10-05 16:33:38 +0200

    0.1

--- app/code/community/PAJ/Customersales/controllers/CustomersalesController.php
+++ app/code/community/PAJ/Customersales/controllers/CustomersalesController.php
@@ -1,19 +1,26 @@
 <?php
+
+//
+// modman clone https://github.com/gaiterjones/magento-customersales.git
+//
+
+
 class PAJ_Customersales_CustomersalesController extends Mage_Adminhtml_Controller_Action
 {
     public function indexAction()
     {
+    
 	
-		//echo $this->getFullActionName();
-		
-		$this->loadLayout()
-			->_setActiveMenu('sales')
-			->_title($this->__('Customer Sales by VAT ID'));
+	//echo $this->getFullActionName();
+	
+	$this->loadLayout()
+		->_setActiveMenu('sales')
+        ->_title($this->__('Customer Sales by VAT ID'));
 
-		
-		$this->_addContent($this->getLayout()->createBlock('adminhtml/template')->setTemplate('paj/customersales/customersales.phtml'));
-		
-		$this->renderLayout();	
+    
+	$this->_addContent($this->getLayout()->createBlock('adminhtml/template')->setTemplate('paj/customersales/customersales.phtml'));
+    
+	$this->renderLayout();	
 		
     }
 }
